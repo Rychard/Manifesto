277cf7f8303a2ff0d5b09311c45341f1e5cb79d0
Joshua Shearer <joshua.a.shearer@gmail.com>
2014-09-16 20:21:07 -0400

    Modified the sample program to include examples for creating manifest files automatically as well as manually.  This needs better documentation (in the form of comments, as I expect developers to be the target audience.)

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Xml.Serialization;
 using Manifesto;
 
@@ -9,47 +10,17 @@ namespace Sample
     class Program
     {
         private static Double lastProgressReportPercentage = 0D;
+        private static String path = "";
+        private static String hashingAlgorithm = "";
 
         static void Main(string[] args)
         {
             // TODO: Read arguments in to generate manifests without requiring user-input.
 
-            var pathDefault = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            Console.WriteLine("Please enter a directory path to generate a manifest. (Press enter for default)");
-            Console.WriteLine("Default Value: \"{0}\"", pathDefault);
-            Console.WriteLine();
-            Console.Write("Path: ");
-            var path = Console.ReadLine();
-            if (String.IsNullOrWhiteSpace(path))
-            {
-                path = pathDefault;
-            }
-            if (!path.EndsWith(Path.DirectorySeparatorChar.ToString(CultureInfo.InvariantCulture)))
-            {
-                path += Path.DirectorySeparatorChar;
-            }
-
-            if (!Directory.Exists(path))
-            {
-                Console.WriteLine("The entered directory was invalid or does not exist.  Exiting...");
-                return;
-            }
+            AcceptUserInput();
             
-            Console.WriteLine();
-            Console.WriteLine("Enter the name of a Hashing Algorithm  (Press enter for default.)");
-            Console.WriteLine("Default Value: \"\" (Skip Hashing)");
-            Console.WriteLine("Examples: MD5, SHA1, SHA256, SHA384, SHA512");
-            Console.WriteLine("A full list of valid values can be found here:");
-            Console.WriteLine(" http://msdn.microsoft.com/en-us/library/wet69s13(v=vs.110).aspx ");
-            Console.WriteLine();
-            Console.Write("Hashing Algorithm: ");
-            var hashingAlgorithm = Console.ReadLine();
-            Console.WriteLine();
-
-	        var m = ManifestHelper.CreateManifest(path, hashingAlgorithm);
-            m.Wait();
-
-            var manifest = m.Result;
+            //var manifest = CreateManifestFromFolder();
+            var manifest = CreateManifestManually();
 
             String desktop = Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop);
             String outputPath = String.Format(@"{0}\Manifest_{1}.xml", desktop, DateTime.UtcNow.ToFileTimeUtc());
@@ -93,6 +64,100 @@ namespace Sample
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        private static void AcceptUserInput()
+        {
+            var pathDefault = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            Console.WriteLine("Please enter a directory path to generate a manifest. (Press enter for default)");
+            Console.WriteLine("Default Value: \"{0}\"", pathDefault);
+            Console.WriteLine();
+            Console.Write("Path: ");
+            path = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(path))
+            {
+                path = pathDefault;
+            }
+            if (!path.EndsWith(Path.DirectorySeparatorChar.ToString(CultureInfo.InvariantCulture)))
+            {
+                path += Path.DirectorySeparatorChar;
+            }
+
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("The entered directory was invalid or does not exist.  Exiting...");
+                return;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the name of a Hashing Algorithm  (Press enter for default.)");
+            Console.WriteLine("Default Value: \"\" (Skip Hashing)");
+            Console.WriteLine("Examples: MD5, SHA1, SHA256, SHA384, SHA512");
+            Console.WriteLine("A full list of valid values can be found here:");
+            Console.WriteLine(" http://msdn.microsoft.com/en-us/library/wet69s13(v=vs.110).aspx ");
+            Console.WriteLine();
+            Console.Write("Hashing Algorithm: ");
+            hashingAlgorithm = Console.ReadLine();
+            Console.WriteLine();
+        }
+
+        private static Manifest CreateManifestFromFolder()
+        {
+            var pathDefault = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            Console.WriteLine("Please enter a directory path to generate a manifest. (Press enter for default)");
+            Console.WriteLine("Default Value: \"{0}\"", pathDefault);
+            Console.WriteLine();
+            Console.Write("Path: ");
+            var path = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(path))
+            {
+                path = pathDefault;
+            }
+            if (!path.EndsWith(Path.DirectorySeparatorChar.ToString(CultureInfo.InvariantCulture)))
+            {
+                path += Path.DirectorySeparatorChar;
+            }
+
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("The entered directory was invalid or does not exist.  Exiting...");
+                return null;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the name of a Hashing Algorithm  (Press enter for default.)");
+            Console.WriteLine("Default Value: \"\" (Skip Hashing)");
+            Console.WriteLine("Examples: MD5, SHA1, SHA256, SHA384, SHA512");
+            Console.WriteLine("A full list of valid values can be found here:");
+            Console.WriteLine(" http://msdn.microsoft.com/en-us/library/wet69s13(v=vs.110).aspx ");
+            Console.WriteLine();
+            Console.Write("Hashing Algorithm: ");
+            var hashingAlgorithm = Console.ReadLine();
+            Console.WriteLine();
+
+            var m = ManifestHelper.CreateManifest(path, hashingAlgorithm);
+            m.Wait();
+
+            var manifest = m.Result;
+            return manifest;
+        }
+
+        private static Manifest CreateManifestManually()
+        {
+            var files = ManifestHelper.GetFiles(path);
+
+            Manifest m = new Manifest();
+            m.AddFiles(files);
+
+            String commonAncestor;
+            m.Contents = Manifest.FindCommonAncestor(m.Contents, out commonAncestor).ToList();
+
+            m.HashAlgorithm = hashingAlgorithm;
+            // Skip files larger than 10MB, for the sake of speed.
+            const long tenMegs = 10485760;
+            m.CalculateHashes(commonAncestor, tenMegs);
+
+            return m;
         }
 
         private static void OnValidationProgressChanged(object sender, ManifestValidatorProgressChangedEventArgs e)
